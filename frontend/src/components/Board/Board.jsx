ffedd92452efecea810f9a0fb48ca450f521639e
radoid17 <60017898+radoid17@users.noreply.github.com>
2024-05-16 21:46:30 +0300

    Add files via upload

--- frontend/src/components/Board/Board.jsx
+++ frontend/src/components/Board/Board.jsx
@@ -1,9 +1,57 @@
+// Board.jsx
+import React, { useState } from "react";
+import Card from "../Card/Card";
+import Cards from "../Cards/Cards";
+
 const Board = () => {
+  const [droppedCards1, setDroppedCards1] = useState([]);
+  const [droppedCards2, setDroppedCards2] = useState([]);
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleDrop1 = (event) => {
+    event.preventDefault();
+    const cardId = event.dataTransfer.getData("text/plain");
+    const draggedCard = document.getElementById(cardId);
+    setDroppedCards1([...droppedCards1, { id: cardId }]);
+    event.target.appendChild(draggedCard);
+  };
+
+  const handleDrop2 = (event) => {
+    event.preventDefault();
+    const cardId = event.dataTransfer.getData("text/plain");
+    const draggedCard = document.getElementById(cardId);
+    setDroppedCards2([...droppedCards2, { id: cardId }]);
+    event.target.appendChild(draggedCard);
+  };
+
   return (
-    <div className="h-[700px] w-[1000px] bg-green-600 flex flex-col justify-between">
-      <div className="bg-gray-800/40 h-[300px] w-fill">Zona carti player 1</div>
+    <div>
+      <div className="h-[350px] w-[1000px] bg-green-600 flex flex-col justify-between">
+        <div
+          className="bg-gray-800/40 h-[600px] w-fill"
+          onDrop={handleDrop1}
+          onDragOver={handleDragOver}
+        >
+          {/* Render cards for drop area 1 */}
+          <Cards cardsArr={droppedCards1} />
+        </div>
+      </div>
+
       <div className="h-2 bg-black" />
-      <div className="bg-gray-800/40 h-[300px] w-fill">Zona carti player 2</div>
+
+      <div className="h-[350px] w-[1000px] bg-green-600 flex flex-col justify-between">
+        <div
+          className="bg-gray-800/40 h-[600px] w-fill"
+          onDrop={handleDrop2}
+          onDragOver={handleDragOver}
+        >
+          {/* Render cards for drop area 2 */}
+          <Cards cardsArr={droppedCards2} />
+        </div>
+      </div>
     </div>
   );
 };
