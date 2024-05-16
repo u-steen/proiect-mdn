2129a91e900db1774e0d1ccf582e421f40099618
radoid17 <60017898+radoid17@users.noreply.github.com>
2024-05-16 21:45:07 +0300

    Add files via upload

--- frontend/src/components/Card/Card.jsx
+++ frontend/src/components/Card/Card.jsx
@@ -1,17 +1,25 @@
+// Card.js
 import React, { useState } from "react";
 
-const Card = ({ name, image_url, health, power }) => {
+const Card = ({ id, name, image_url, health, power }) => {
   const [isHovered, setIsHovered] = useState(false);
+
   const handleHover = () => {
     setIsHovered(!isHovered);
   };
+
   return (
     <div
+      id={id} // Adding unique id for each card
       className={`h-[250px] w-[180px] bg-green-800 relative border-2 border-black/20 transition-transform duration-100 ${
         isHovered && "-translate-y-12 z-10"
       }`}
       onMouseEnter={handleHover}
       onMouseLeave={handleHover}
+      draggable // Allowing the card to be draggable
+      onDragStart={(event) => {
+        event.dataTransfer.setData("text/plain", id); // Setting the dragged card's id
+      }}
     >
       <div className="absolute top-0 left-0 h-12 w-12 rounded-full bg-red-500 flex justify-center items-center">
         <h4>{health}</h4>
