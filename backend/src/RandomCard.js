4d63191a1a6468497f8fbb7bf79eee38110eadd5
AlexandraVlaicu <127432626+AlexandraVlaicu@users.noreply.github.com>
2024-05-20 15:25:37 +0300

    Update RandomCard.js

--- backend/src/RandomCard.js
+++ backend/src/RandomCard.js
@@ -1,13 +1,27 @@
-
-const cards = require('./cards');
-
+const cards = require('./Cards');
 const crypto = require('crypto');
 
-function selectRandomCard() {
-    const randomIndex = crypto.randomInt(0, cards.length);
 
-    return cards[randomIndex];
+function selectRandomCard(deck) {
+    const randomIndex = crypto.randomInt(0, deck.length);
+    const selectedCard = deck[randomIndex];
+    deck.splice(randomIndex, 1); // Elimin cartea din deck
+    return selectedCard;
 }
 
-const randomCard = selectRandomCard();
-console.log("Randomly selected card:", randomCard);
+const deck = [...cards]; // Copiem array-ul de carti
+
+function drawAndLogCard() {
+    if (deck.length === 0) {
+        console.log("No more cards in the deck!");
+        return;
+    }
+
+    const randomCard = selectRandomCard(deck);
+    console.log("Randomly selected card:", randomCard);
+}
+drawAndLogCard();
+drawAndLogCard();
+drawAndLogCard();
+drawAndLogCard();
+drawAndLogCard();
