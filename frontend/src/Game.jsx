1ebf383e8c18417271706847db8f39109bad0875
radoid17 <60017898+radoid17@users.noreply.github.com>
2024-05-20 13:12:11 +0300

    Add files via upload

--- frontend/src/Game.jsx
+++ frontend/src/Game.jsx
@@ -7,92 +7,113 @@ import Healthbar from "./components/Healthbar/Healthbar";
 import ManaBar from "./components/ManaBar/ManaBar";
 
 function Game() {
-  const defaultCards = [
-    {
-      id: 1,
-      name: "gigel",
-      health: 4,
-      power: 2,
-    },
-    {
-      id: 2,
-      name: "mirel",
-      health: 1,
-      power: 1,
-    },
-    {
-      id: 3,
-      name: "vasile",
-      health: 1,
-      power: 1,
-    },
-    {
-      id: 4,
-      name: "george",
-      health: 1,
-      power: 1,
-    },
-    {
-      id: 5,
-      name: "florin",
-      health: 1,
-      power: 1,
-    },
+  //Ii dam lui Player1 un set de carti
+  const defaultWalterCards = [
+    { id: 1, name: "gigel", health: 4, power: 2 },
+    { id: 2, name: "mirel", health: 1, power: 1 },
+    { id: 3, name: "vasile", health: 1, power: 1 },
+    { id: 4, name: "george", health: 1, power: 1 },
+    { id: 5, name: "florin", health: 1, power: 1 },
   ];
-  const [cards, setCards] = useState(defaultCards);
-  const [walterMana, setWalterMana] = useState(20); // Mana lui Walter
-  const [jesseMana, setJesseMana] = useState(20); // Mana lui Jesse
-  const [walterHealth, setWalterHealth] = useState(75); // HealthBar-ul lui Walter
-  const [jesseHealth, setJesseHealth] = useState(50); // HealthBar-ul lui Jesse
 
+  //Ii dam lui Player2 un set de carti
+  const defaultJesseCards = [
+    { id: 6, name: "jesse_card1", health: 3, power: 2 },
+    { id: 7, name: "jesse_card2", health: 2, power: 2 },
+    { id: 8, name: "jesse_card3", health: 1, power: 1 },
+    { id: 9, name: "jesse_card4", health: 1, power: 1 },
+    { id: 10, name: "jesse_card5", health: 1, power: 1 },
+  ];
+
+  //Declaram cata mana, health au playeri si care player incepe
+  const [walterCards, setWalterCards] = useState(defaultWalterCards);
+  const [jesseCards, setJesseCards] = useState(defaultJesseCards);
+  const [walterMana, setWalterMana] = useState(20);
+  const [jesseMana, setJesseMana] = useState(20);
+  const [walterHealth, setWalterHealth] = useState(75);
+  const [jesseHealth, setJesseHealth] = useState(50);
+  const [currentPlayer, setCurrentPlayer] = useState('Walter');
+
+  //functie pentru a putea trage carti din deck
   const handleDraw = (card) => {
-    if (walterMana >= 2) {
-      setWalterMana(prevMana => prevMana - 2);
-      setCards([...cards, card]);
+    //veridicam cata mana au player1 si player2
+    if (currentPlayer === 'Walter' && walterMana >= 2) {
+      const newWalterMana = Math.min(walterMana - 2, 20);
+      //costul pentru a trage o carte din deck este de 2 mana
+      setWalterMana(newWalterMana);
+      setWalterCards([...walterCards, card]);
+    } else if (currentPlayer === 'Jesse' && jesseMana >= 2) {
+      const newJesseMana = Math.min(jesseMana - 2, 20);
+      //costul pentru a trage o carte din deck este de 2 mana
+      setJesseMana(newJesseMana);
+      setJesseCards([...jesseCards, card]);
     } else {
       console.log("Not enough mana to draw a card!");
     }
   };
 
   const handleRemoveCard = (cardId) => {
-    setCards(cards.filter(card => card.id !== cardId));
+    if (currentPlayer === 'Walter') {
+      setWalterCards(walterCards.filter(card => card.id !== cardId));
+    } else {
+      setJesseCards(jesseCards.filter(card => card.id !== cardId));
+    }
   };
 
+  //Functia care verifica a cui este tura
   const handleEndTurn = () => {
     console.log("End turn");
-    setWalterMana(prevMana => prevMana + 5); // Adauga 5 mana lui Walter dupa o tura
-    setJesseMana(prevMana => prevMana + 5); // Adauga 5 mana lui Jesse dupa o tura
+    if (currentPlayer === 'Walter') {
+      //Player1 primeste 5 mana odata ce i se termina tura pentru a putea sa faca alte actiuni
+      const newWalterMana = Math.min(walterMana + 5, 20);
+      setWalterMana(newWalterMana);
+      //Vine randul lui player2
+      setCurrentPlayer('Jesse');
+    } else {
+      //Player2 primeste 5 mana odata ce i se termina tura pentru a putea sa faca alte actiuni
+      const newJesseMana = Math.min(jesseMana + 5, 20);
+      setJesseMana(newJesseMana);
+      //Vine randul lui player1
+      setCurrentPlayer('Walter');
+    }
   };
 
   return (
     <div className="h-[100vh] w-[100vw] bg-blue-200 mx-auto flex justify-center pt-12 relative overflow-hidden">
-      <Board cards={cards} onRemoveCard={handleRemoveCard} />
+      <div className="absolute left-10 top-20 bg-white p-4 rounded shadow-md">
+        <p className="text-xl font-bold">Current Player:</p>
+        <p className="text-2xl">{currentPlayer}</p>
+      </div>
+      <Board cards={currentPlayer === 'Walter' ? walterCards : jesseCards} onRemoveCard={handleRemoveCard}
+        currentPlayer={currentPlayer} // Asigură-te că transmiti propria tură curentă
+      />
+
       <div className="absolute right-10 top-40">
-        <Deck drawCallback={handleDraw} disabled={walterMana < 2} />
+        <Deck drawCallback={handleDraw} disabled={(currentPlayer === 'Walter' && walterMana < 2) || (currentPlayer === 'Jesse' && jesseMana < 2)} />
       </div>
       <div className="absolute -bottom-40">
-        <Cards cardsArr={cards} />
+        <Cards cardsArr={currentPlayer === 'Walter' ? walterCards : jesseCards} />
       </div>
       <div className="absolute top-[420px] right-12">
-        <EndTurnButton endTurnCallback={handleEndTurn} /> {/* Pass the end turn callback */}
+        <EndTurnButton endTurnCallback={handleEndTurn} />
       </div>
 
       <div className="absolute bottom-20 right-20">
-        Walter
-        <Healthbar health={walterHealth} /> {/* Pass Walter's health */}
+        {currentPlayer === 'Walter' ? 'Walter' : 'Jesse'}
+        <Healthbar health={currentPlayer === 'Walter' ? walterHealth : jesseHealth} />
       </div>
       <div className="absolute bottom-40 right-20">
         Mana
-        <ManaBar mana={walterMana} /> {/* Pass Walter's mana */}
+        <ManaBar mana={currentPlayer === 'Walter' ? walterMana : jesseMana} />
       </div>
 
       <div className="absolute top-20 right-20">
-        Jesse
-        <Healthbar health={jesseHealth} /> {/* Pass Jesse's health */}
+        {currentPlayer === 'Walter' ? 'Jesse' : 'Walter'}
+        <Healthbar health={currentPlayer === 'Walter' ? jesseHealth : walterHealth} />
       </div>
       <div className="absolute top-10 right-20">
         Mana
-        <ManaBar mana={jesseMana} /> {/* Pass Jesse's mana */}
+        <ManaBar mana={currentPlayer === 'Walter' ? jesseMana : walterMana} />
       </div>
     </div>
   );
